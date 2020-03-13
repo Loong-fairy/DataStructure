229eb2ab9ec4ac739f395a5ae3b7f96ae5b50e19
long <fjllove5@qq.com>
2020-03-13 09:04:07 +0800

    '顶点类

---
+++ 代码/DS5-graph/graphPractise.py
@@ -0,0 +1,63 @@
+"""
+    字典: connectedTo
+    addNeighbor方法用于从这个顶点添加一个连接到另一个顶点, 构造顶点
+    getConnections方法返回邻接表中的所有的点
+    getWegiht方法返回权重
+"""
+
+
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + 'connectedTo:' + str([x.id for x in self.connectedTo])
+
+    def getId(self):
+        return self.id
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getWegiht(self, nbr):
+        return self.connectedTo[nbr]
+
+
+# 图
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def addEdge(self, fromVert, toVert, weight=0):
+        if fromVert not in self.vertList:
+            nv = self.addVertex(fromVert)
+        if toVert not in self.vertList:
+            nv = self.addVertex(toVert)
+
+        self.vertList[fromVert].addNeighbor(self.vertList[toVert], weight)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+
+
+
+
+
